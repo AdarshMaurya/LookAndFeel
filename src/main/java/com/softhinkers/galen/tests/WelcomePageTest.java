ea73189c007619e32034a1fb964c03568acf0586
AdarshMaurya <adarshmauryatalks@gmail.com>
2016-10-04 05:10:21 +0530

    updates path changed for checkLayout

--- src/main/java/com/softhinkers/galen/tests/WelcomePageTest.java
+++ src/main/java/com/softhinkers/galen/tests/WelcomePageTest.java
@@ -6,22 +6,24 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import com.softhinkers.galen.components.GalenTestBase;
-import com.softhinkers.galen.components.GalenTestBase.TestDevice;
-
 
 public class WelcomePageTest extends GalenTestBase {
 
-    @Test(dataProvider = "devices")
-    public void welcomePage_shouldLookGood_onDevice(TestDevice device) throws IOException {
-        load("/");
-        checkLayout("/specs/welcomePage.spec", device.getTags());
-    }
+	@Test(dataProvider = "devices")
+	public void welcomePage_shouldLookGood_onDevice(TestDevice device)
+			throws IOException {
+		load("/");
+		checkLayout("./src/main/resources/specs/welcomePage.spec", device.getTags());
+	}
 
-    @Test(dataProvider = "devices")
-    public void loginPage_shouldLookGood_onDevice(TestDevice device) throws IOException {
-        load("/");
-        getDriver().findElement(By.xpath("//button[.='Login']")).click();
-        checkLayout("/specs/loginPage.spec", device.getTags());
-    }
+	@Test(dataProvider = "devices")
+	public void loginPage_shouldLookGood_onDevice(TestDevice device)
+			throws IOException {
+		load("/");
+		getDriver().findElement(By.xpath("//button[.='Login']")).click();
+		checkLayout("./src/main/resources/specs/loginPage.spec", device.getTags());
+	}
+
+	
 
 }
